6b3422aec6aa3f985797356bb58a4d646224ad43
Michael Rowsey <mike@rowsey.org>
2025-12-11 16:03:10 +0000

    Remove test function and simplify main.c to print "Hello, World!"

--- src/main.c
+++ src/main.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "util.h"
-#include "testes.h"
 
 int main(void)
 {
-    test_me();
+    printf("Hello, World!\n");
     return 0;
 }
--- src/testes.c
+++
@@ -1,7 +0,0 @@
-#include "testes.h"
-#include <stdio.h>
-
-void test_me(void)
-{
-    printf("Test function executed successfully.\n");
-}
